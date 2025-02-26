fb3ae63b4fb2f2fdb3a1ef5376733e1a15d7f6b5
Alge4 <alge4@hotmail.co.uk>
2025-02-26 22:56:09 +0000

    Initial Js implementation startup

--- tests/setup.js
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-
-// Mock fetch globally
-global.fetch = jest.fn();
-
-// Clean up after each test
-afterEach(() => {
-  jest.clearAllMocks();
-});
--- update_requirements.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def update_requirements():
-    os.system('pip freeze > requirements.txt')
-
-if __name__ == '__main__':
-    update_requirements()
